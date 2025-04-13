292e02b1ff419d14e2106a39d1e8d48261af4aa2
Andrii Kurdiumov <kant2002@gmail.com>
2025-04-13 23:34:35 +0500

    Add Github pages deployment (#1)
    * Add Github pages deployment
    * Add Source link and version to 0.0.1
    * Add readme to package

--- WebSql/WebSqlInterop.cs
+++ WebSql/WebSqlInterop.cs
@@ -11,24 +11,28 @@ public class WebSqlInterop : IAsyncDisposable
         moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/WebSql/sql-launcher.js").AsTask());
     }
 
+    /// <summary>
+    /// Creates a new empty Sqlite database asynchronously.
+    /// </summary>
+    /// <returns>Returns a ValueTask containing a WebSqlDatabase instance.</returns>
     public async ValueTask<WebSqlDatabase> CreateDatabase()
     {
         var module = await moduleTask.Value;
         return new(await module.InvokeAsync<IJSObjectReference>("createDatabase"));
     }
 
+    /// <summary>
+    /// Creates a WebSqlDatabase instance from a specified SQLite remote location asynchronously.
+    /// </summary>
+    /// <param name="sqliteFileLocation">Specifies the remote location of the SQLite file to be used for creating the database.</param>
+    /// <returns>Returns a WebSqlDatabase object created from the provided SQLite file.</returns>
     public async ValueTask<WebSqlDatabase> CreateDatabaseFromFile(string sqliteFileLocation)
     {
         var module = await moduleTask.Value;
         return new(await module.InvokeAsync<IJSObjectReference>("createDatabaseFromFile", sqliteFileLocation));
     }
 
-    public async ValueTask Exec(WebSqlDatabase db, string sql)
-    {
-        var module = await moduleTask.Value;
-        await module.InvokeVoidAsync("exec", db.objectReference, sql);
-    }
-
+    /// <inheritdoc />
     public async ValueTask DisposeAsync()
     {
         if (moduleTask.IsValueCreated)
